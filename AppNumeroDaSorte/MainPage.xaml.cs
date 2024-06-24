2ed6dc8d527ba0831c3c9da6eb1de6824c360241
Reinaldo G. P. Neto <reinaldogpn@outlook.com>
2024-06-24 19:37:07 -0300

    Logics finished

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace AppNumeroDaSorte;
+﻿using System.Collections.Generic;
+
+namespace AppNumeroDaSorte;
 
 public partial class MainPage : ContentPage
 {
@@ -11,5 +13,29 @@ public partial class MainPage : ContentPage
 	{
         AppName.IsVisible = false;
 		ContainerLuckyNumbers.IsVisible = true;
+
+		var set = GenerateLuckyNumbers();
+
+		LuckyNumber01.Text = set.ElementAt(0).ToString("D2");
+        LuckyNumber02.Text = set.ElementAt(1).ToString("D2");
+        LuckyNumber03.Text = set.ElementAt(2).ToString("D2");
+        LuckyNumber04.Text = set.ElementAt(3).ToString("D2");
+        LuckyNumber05.Text = set.ElementAt(4).ToString("D2");
+        LuckyNumber06.Text = set.ElementAt(5).ToString("D2");
+    }
+
+	private SortedSet<int> GenerateLuckyNumbers()
+	{
+        var set = new SortedSet<int>();
+
+		while (set.Count < 6)
+		{
+			var random = new Random();
+			var luckyNumber = random.Next(1, 60);
+
+			set.Add(luckyNumber);
+		}
+
+		return set;
 	}
 }
