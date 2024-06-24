b893a9b715af39ad29222a11575277fe78d44313
Reinaldo G. P. Neto <reinaldogpn@outlook.com>
2024-06-24 19:24:50 -0300

    Building logics

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -2,8 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	private void OnGenerateLuckyNumbers(Object sender, EventArgs e)
+	{
+        AppName.IsVisible = false;
+		ContainerLuckyNumbers.IsVisible = true;
 	}
 }
